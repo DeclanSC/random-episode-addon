315021b8b0e98f283afbf0200df92b2eebb07f78
DeclanSC <140628807+DeclanSC@users.noreply.github.com>
2025-11-24 00:27:48 +0000

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,14 @@
+{ pkgs }: {
+  deps = [
+    pkgs.nodejs-18_x
+    pkgs.yarn
+    pkgs.nodePackages.npm
+    pkgs.python3
+  ];
+
+  env = {
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.stdenv.cc.cc.lib
+    ];
+  };
+}
